0bfdefcc67ecb91733b6ade71b71c018287ed741
Hiren Solanki <71432656+hiren-solanki1@users.noreply.github.com>
2024-11-11 00:21:03 +0530

    path bugfix

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from .routes import main
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config.from_object(Config)
     init_db(app)
     app.register_blueprint(main)
